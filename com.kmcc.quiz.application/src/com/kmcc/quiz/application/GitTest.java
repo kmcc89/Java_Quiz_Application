651c4fe2eaf6f226b321513438f3418d0862a7e7
Kevin Mc Carthy <mccarthyk@avaya.com>
2019-04-15 12:49:12 +0100

    Initial Commit

---
+++ com.kmcc.quiz.application/src/com/kmcc/quiz/application/GitTest.java
@@ -0,0 +1,10 @@
+package com.kmcc.quiz.application;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Push to Git");
+	}
+
+}
---
+++ com.kmcc.quiz.application/src/com/kmcc/quiz/application/GitTest2.java
@@ -0,0 +1,9 @@
+package com.kmcc.quiz.application;
+
+public class GitTest2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Second class for git");
+	}
+}
